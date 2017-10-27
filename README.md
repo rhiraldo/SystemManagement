6bf9d6156ad3542994ab1d5d00d1da684bdd1a7a
Richard Hiraldo <rhiraldo@diligent.com>
2017-10-26 23:29:37 -0400

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# SystemManagement
+System Management Module
+========================
+
+Instructions
+------------
+Improve the following script on every possible way having in mind the following non-functional requirements:
+
+Scalability
+Robustness
+Capacity
+Reliability
+Maintainability
+Security
+Usability
+Interoperability
+
+The idea is that you need to be able to perform reboots, sometimes to multiple servers at the same time.
+In case of failure, send an email to systemadmins@diligent.com with the list of servers.
--- Reboot-Servers.ps1
+++ Reboot-Servers.ps1
@@ -1,19 +1,4 @@
-﻿<#
-Improve the following script on every possible way having in mind the following non-functional requirements:
-
-Scalability
-Robustness
-Capacity
-Reliability
-Maintainability
-Security
-Usability
-Interoperability
-
-The idea is that you need to be able to perform reboots, sometimes to multiple servers at the same time.
-In case of failure, send an email to systemadmins@diligent.com with the list of servers.
-#>
-$ScriptPath = Split-Path $MyInvocation.MyCommand.Path
+﻿$ScriptPath = Split-Path $MyInvocation.MyCommand.Path
 Import-Module $ScriptPath\reboot.psm1
 $menu = @”
 Press 1 to reboot servera
@@ -25,12 +10,21 @@ Press 3 to reboot servera and serverb
 $choice = Read-Host -Prompt $menu
 
 switch ($choice) {
-1 {Reboot $servera; break}
-2 {Reboot $serverb; break}
-3 {
-Reboot $servera
-Reboot $serverb
-break
-}
-default{Write-Warning "Nothing rebooted"}
+    1 {
+        Reboot $servera
+        break
+      }
+    2 {
+        Reboot $serverb
+        break
+      }
+    3 {
+        Reboot $servera
+        Reboot $serverb
+        break
+      }
+    default
+    {
+        Write-Warning "Nothing rebooted"
+    }
 }
